919ac8c99024e259fb4518c2de2fe69cc9e46cc4
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-06-18 19:45:35 -0300

    começo exercicios tuplas

--- Orientação a Objetos - Python/03-intro-oo/exercicios_oo/exercicios_tuplas_dicionarios_sets.py
+++ Orientação a Objetos - Python/03-intro-oo/exercicios_oo/exercicios_tuplas_dicionarios_sets.py
@@ -1,23 +1,14 @@
+#1
 def leTupla(tupla):
     print(tupla);
 
-# tupla = ("teste", 1 , 1.5);
-# leTupla(tupla);
+#2
+def populandoTupla():
+    lista = []
+    for i in range(0,5):
+        numero = input("Inserir numero: ");
+        lista.append(numero);
+    tupla = tuple(lista);
+    leTupla(tupla);
 
-# tupla = ();
-
-# for i in range(0,5):
-#     numero = int(input("Inserir numero: "));
-#     tupla = numero;
-
-# leTupla(tupla);
-
-INDICE = 13;
-SOMA = 0;
-K = 0;
-
-while K < INDICE:
-    K = K + 1;
-    SOMA = SOMA + K;
-
-print(SOMA);
+#3
